6b2bc8a9ccbbcdb3730553edc2b93f6bb333b210
DonXI <68387776+DonXI@users.noreply.github.com>
2022-09-24 03:04:54 +0700

    new version

--- check_image.py
+++ check_image.py
@@ -1,10 +1,13 @@
-# before run code please install Duplicate Image Folder "difPy"
+# before run code please install Duplicate Image Finder >>> pip install difPy
 # ref : https://github.com/elisemercury/Duplicate-Image-Finder
-# pip install difPy
+# document : https://pypi.org/project/difPy/1.2/
+#            https://github.com/elisemercury/Duplicate-Image-Finder/wiki/difPy-Usage-Documentation
 
 from difPy import dif
 
-search = dif("path_folder_imageA", "path_folder_imageB")
+dif("D:/pic_fish/1image_fish", "D:/pic_fish/2image_fish",similarity="normal", delete=True)
 
-print(search.result) # compare the image 2 folder show duplicate images
+#print(search.result)
+#print(search.lower_quality)
+#print(search.stats)
 
