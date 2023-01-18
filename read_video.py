7ff36932288c12a596191b75335c0ee60cc36932
DonXI <s6201012620082@email.kmutnb.ac.th>
2023-01-19 00:59:27 +0700

    read VDO

---
+++ read_video.py
@@ -0,0 +1,24 @@
+import cv2, os
+
+num = 0
+limit_img = 10
+name_folder = "img"
+cap = cv2.VideoCapture("video.mp4") 
+
+# crate new folder 
+if not os.path.isdir(name_folder):
+    os.makedirs(name_folder)
+
+while (cap.isOpened) and num <= limit_img:
+    # read video 
+    check, frame = cap.read()
+    # check video is not finish
+    if check == True:
+
+        # save file image
+        cv2.imwrite(f"{name_folder}/IMG{num}.jpg", frame)
+    else:
+        break
+    num += 1
+    
+cap.release()
